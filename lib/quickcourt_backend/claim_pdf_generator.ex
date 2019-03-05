6059413e6e5d70cfd20b6a3d7fe9f0e3e4231cd3
David Majercak <david1majercak@gmail.com>
2019-03-05 09:27:13 +0100

    REFACTOR: updated intendation

--- lib/quickcourt_backend/claim_pdf_generator.ex
+++ lib/quickcourt_backend/claim_pdf_generator.ex
@@ -1,31 +1,35 @@
 defmodule QuickcourtBackend.ClaimPdfGenerator do
-
   alias EEx
 
-
   def generate_pdf(claim) do
-    claim_list = Map.from_struct(claim)
-    |> Enum.filter(fn {k, v} -> !Enum.member?(["__meta__", "inserted_at", "updated_at"],  Atom.to_string(k)) && v != nil end)
-    |> Enum.map(fn {k, v} -> 
-      new_value = case v do
-        %DateTime{} -> to_string(v)
-        %{} -> to_string(v.name)
-        val -> to_string(val)
-      end
-      {k, new_value}
-    end)
+    claim_list =
+      Map.from_struct(claim)
+      |> Enum.filter(fn {k, v} ->
+        !Enum.member?(["__meta__", "inserted_at", "updated_at"], Atom.to_string(k)) && v != nil
+      end)
+      |> Enum.map(fn {k, v} ->
+        new_value =
+          case v do
+            %DateTime{} -> to_string(v)
+            %{} -> to_string(v.name)
+            val -> to_string(val)
+          end
+
+        {k, new_value}
+      end)
+
     html = EEx.eval_file("./assets/53C.1-European_Small_Claims_Form_A.html", claim_list)
 
     try do
       {:ok, filename} =
-        PdfGenerator.generate html, page_size: "A4", shell_params: ["--dpi", "300"]
+        PdfGenerator.generate(html, page_size: "A4", shell_params: ["--dpi", "300"])
 
       {:ok, file_contents} = File.read(filename)
       Base.encode64(file_contents)
     rescue
-      e -> 
+      e ->
         IO.inspect(e)
-        throw e
+        throw(e)
     end
   end
 end
