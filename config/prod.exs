4274aa003d0184307c40d58525e1cfb582a781f0
David Majercak <david1majercak@gmail.com>
2019-03-19 22:09:54 +0100

    BUILD: ignore prod secret file

--- config/prod.exs
+++ config/prod.exs
@@ -89,4 +89,4 @@ config :quickcourt_backend, QuickcourtBackend.Repo,
 
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
